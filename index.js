8cd078c966328428ff583dd64ac6aaa12ef35c5d
Serhii <serhii.jun@gmail.com>
2023-07-23 20:51:59 +0300

    Fix exports bug

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export { ActorForward } from "./src/actor_forward";
-export { ActorBase } from "./src/actor_base";
-export { ActorElement } from "./src/actor_element";
+import { ActorForward } from "./src/actor_forward";
+import { ActorBase } from "./src/actor_base";
+import { ActorElement } from "./src/actor_element";
+
+export {
+  ActorForward,
+  ActorBase,
+  ActorElement
+};
