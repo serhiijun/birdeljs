1313e85b431d140704f85fd2a757d11730e75510
Serhii <serhii.jun@gmail.com>
2023-08-22 02:51:25 +0300

    Bla

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ActorForward } from "./src/actor_forward";
-import { ActorBase } from "./src/actor_base";
+import { ActorBase }    from "./src/actor_base";
 import { ActorElement } from "./src/actor_element";
-import { BFormData } from "./src/components/actor_form";
+import { BFormData }    from "./src/components/actor_form";
 
 export {
   ActorForward,
--- src/actor_base.js
+++ src/actor_base.js
@@ -1,31 +1,29 @@
 export class ActorBase{
   constructor(element, {setProps} = {setProps: false}){
-    this.element = element;
-    this.name = this.element.dataset.controller;
-    this.resourceId = this.element.dataset.resourceId || false;
-    console.log("setProps", setProps)
-    const retrieveCookie = this.retrievePropsFromCookie({name: this.name});
-    console.log("retrieveCookie", retrieveCookie);
-    this.props = setProps && retrieveCookie;
-    window.Birdel.addActor(this);
+    this.element = element
+    this.name = this.element.dataset.controller
+    this.resourceId = this.element.dataset.resourceId || false
+    this.props = setProps && this.#retrievePropsFromCookie({name: this.name})
+    window.Birdel.addActor({...this})
   }
 
   replace(newElHtml, oldEl){
-    const parser = new DOMParser();
-    const newEl = parser.parseFromString(newElHtml, "text/html").body.firstChild;
-    oldEl.parentNode.replaceChild(newEl, oldEl);
+    const parser = new DOMParser()
+    const newEl = parser.parseFromString(newElHtml, "text/html").body.firstChild
+    oldEl.parentNode.replaceChild(newEl, oldEl)
   }
 
-  retrievePropsFromCookie({name}){
+  #retrievePropsFromCookie({name}){
     const cookieValue = document.cookie
       .split(';')
       .map(cookie => cookie.trim())
-      .find(cookie => cookie.startsWith(`${name}=`));
-    console.log("cookieValue", cookieValue);
-    const decoded = decodeURIComponent(cookieValue.split('=')[1]);
-    console.log("decoded", decoded);
-    const parsedData = JSON.parse(decoded);
-    console.log("parsedData", parsedData);
-    return parsedData;
+      .find(cookie => cookie.startsWith(`${name}=`))
+
+    if (!cookieValue) return false
+
+    const decoded = decodeURIComponent(cookieValue.split('=')[1])
+    const parsedData = JSON.parse(decoded)
+    console.log("parsedData", parsedData)
+    return parsedData
   }
 }
