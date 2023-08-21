b711933baf367e2fc5ac7f1e40b935329edf8367
Serhii <serhii.jun@gmail.com>
2023-08-22 01:44:46 +0300

    Add: feature props from cookies in actors initializer and bApp proxy to access window.Birdel

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import { ActorForward } from "./src/actor_forward";
 import { ActorBase } from "./src/actor_base";
 import { ActorElement } from "./src/actor_element";
 import { BFormData } from "./src/components/actor_form";
-
+const bApp = window.Birdel;
 export {
   ActorForward,
   ActorBase,
   ActorElement,
-  BFormData
+  BFormData,
+  bApp
 };
--- src/actor_base.js
+++ src/actor_base.js
@@ -1,8 +1,9 @@
 export class ActorBase{
-  constructor(element) {
+  constructor(element, {setProps} = {setProps: false}){
     this.element = element;
     this.name = this.element.dataset.controller;
     this.resourceId = this.element.dataset.resourceId || false;
+    this.props = setProps && this.#retrievePropsFromCookie({name: this.name});
     window.Birdel.addActor(this);
   }
 
@@ -10,5 +11,15 @@ export class ActorBase{
     const parser = new DOMParser();
     const newEl = parser.parseFromString(newElHtml, "text/html").body.firstChild;
     oldEl.parentNode.replaceChild(newEl, oldEl);
+  }
+
+  #retrievePropsFromCookie({name}){
+    const cookieValue = document.cookie
+      .split(';')
+      .map(cookie => cookie.trim())
+      .find(cookie => cookie.startsWith(`${name}=`));
+    const decoded = decodeURIComponent(cookieValue.split('=')[1]);
+    const parsedData = JSON.parse(decoded);
+    return parsedData;
   }
 }
